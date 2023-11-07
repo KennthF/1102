daa582b62d1eb2b8f25b1974b5cea4779d10281e
Kenneth <kenneth.factor@edu.sait.ca>
2023-11-06 19:16:20 -0700

    Sort the number and get the missing num.

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ for i in content:
     if i.isnumeric(): #Turn it into a number
         values.append(int(i))
 
-print(values)
+new_file = open("sorted_numbers.txt", "w") #Write a new file
 
-print("\n")
+#Sort the file and get the missing number
+for i in range(min(values), max(values) + 1):
+    if i in values:
+        new_file.write(f"{i} is not missing.\n")
 
-print(content)
+    else:
+        new_file.write(f"{i} is missing.\n")
+
+new_file.close()
+
+with open("sorted_numbers.txt", "r") as sorted_file:
+    print(sorted_file.read())
 
 
  
